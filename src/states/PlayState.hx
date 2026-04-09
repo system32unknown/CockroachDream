6ac9b7e574f4a61041e5d54e8c11f1802e5cbe6c
Altertoriel <79248736+system32unknown@users.noreply.github.com>
2026-04-09 16:00:44 +0900

    some debug

--- src/states/PlayState.hx
+++ src/states/PlayState.hx
@@ -1,16 +1,16 @@
 package states;
 
 class PlayState extends flixel.FlxState {
-	var stress = 0;
-	var stress_max = 800;
-	var goki_touch = 0;
-	var face_lev = -1;
-	var goki_dead = 0;
-	var gameover = 0;
-	var beat_interval = 7;
-	var appear_interval = 80;
-	var appear_cnt = 0;
-	var goki_num = 0;
+	var stress:Int = 0;
+	var stress_max:Int = 800;
+	var goki_touch:Int = 0;
+	var face_lev:Int = -1;
+	var goki_dead:Int = 0;
+	var gameover:Int = 0;
+	var beat_interval:Int = 7;
+	var appear_interval:Int = 80;
+	var appear_cnt:Int = 0;
+	var goki_num:Int = 0;
 	var gokis = [];
 
 	override public function create() {
@@ -19,5 +19,9 @@ class PlayState extends flixel.FlxState {
 
 	override public function update(elapsed:Float) {
 		super.update(elapsed);
+	}
+
+	public static function hitTest():Bool {
+		return false;
 	}
 }
